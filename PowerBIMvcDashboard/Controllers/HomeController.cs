2d7ad369bdf5a8b06fcfc9aaf12e8cd0eca52868
YSwapnilLDS <swapnil.yadav@ap.linedata.com>
2025-07-22 15:15:53 +0530

    Add files via upload

---
+++ PowerBIMvcDashboard/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PowerBIMvcDashboard.Models;
+
+namespace PowerBIMvcDashboard.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        //return View();
+
+        string reportUrl = "https://app.powerbi.com/view?r=eyJrIjoiYWI4NzE1MGYtOTNhMi00MzhjLWJiN2ItMTE3ZjM4NGJiZThhIiwidCI6ImQ4MjY2NjRmLTE1YjUtNDY1ZS1iNzg5LTY1YzNjNjFlNjllZSIsImMiOjN9"; // from "Publish to Web"
+        ViewBag.ReportUrl = reportUrl;
+        ViewBag.RefreshInterval = 3600000; // 5 minutes in milliseconds
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
